6cfa38382dc24f6e767fe6eb479d8f4e51f26661
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-16 16:04:23 -0300

    test: book description

--- tests/descricao_livro/test_descricao_livro.py
+++ tests/descricao_livro/test_descricao_livro.py
@@ -1,5 +1,17 @@
-# from src.livro.livro import Livro
+from src.livro.livro import Livro
 
 
-def test_descricao_livro():
-    pass  # Colocar sua implementação aqui
+def test_descricao_livro(capsys):
+    livro = Livro("Sul da fronteira, oeste do sol", "Haruki Murakami", 224)
+    assert livro.titulo == "Sul da fronteira, oeste do sol"
+    assert livro.autor == "Haruki Murakami"
+    assert livro.paginas == 224
+
+    print(livro)
+
+    out, _ = capsys.readouterr()
+
+    assert (
+        out.strip()
+        == "O livro Sul da fronteira, oeste do sol de Haruki Murakami possui 224 páginas."
+    )
