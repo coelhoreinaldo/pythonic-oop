322ade426a228bf9b329e49da1d8e7581dcaf7b2
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-16 15:48:01 -0300

    feat: req 2, class Estoque

--- src/estoque.py
+++ src/estoque.py
@@ -2,19 +2,35 @@ from typing import Dict
 
 
 class Estoque:
+    produtos: Dict[str, int]
+
     def __init__(self, produtos: Dict[str, int]) -> None:
-        pass
+        self.produtos = produtos
 
     def adicionar_produto_no_estoque(self, nome: str, quantidade: int) -> None:
-        pass
+        if nome in self.produtos:
+            self.produtos[nome] += quantidade
+            return
+
+        self.produtos = {nome: quantidade}
 
     def remover_produto_do_estoque(self, nome: str, quantidade: int) -> None:
-        pass
+        if quantidade > self.produtos[nome]:
+            raise ValueError(
+                "Quantidade a ser removida maior que a quantidade em estoque."
+            )
+
+        self.produtos[nome] -= quantidade
 
     def atualizar_produto_no_estoque(
-        self, nome: str, nova_quantidade: int
+        self,
+        nome: str,
+        nova_quantidade: int,
     ) -> None:
-        pass
+        if nome not in self.produtos:
+            raise ValueError("Produto não encontrado no estoque.")
+
+        self.produtos[nome] = nova_quantidade
 
     def visualizar_estoque(self) -> None:
-        pass
+        return self.produtos
