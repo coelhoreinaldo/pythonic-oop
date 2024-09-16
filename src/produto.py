a3b94a57f09d9bffaedba64a7dc92e7332c8550a
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-16 15:44:15 -0300

    feat: req 1, class Produto

--- src/produto.py
+++ src/produto.py
@@ -1,8 +1,16 @@
 class Produto:
+    _nome: str
+    _codigo: str
+    _preco: float
+    _quantidade: int
+
     def __init__(
         self, nome: str, codigo: str, preco: float, quantidade: int
     ) -> None:
-        pass
+        self._nome = nome
+        self._codigo = codigo
+        self._preco = preco
+        self._quantidade = quantidade
 
     def get_preco(self) -> float:
         pass
@@ -11,10 +19,18 @@ class Produto:
         pass
 
     def atualizar_preco_do_produto(self, novo_preco: float) -> None:
-        pass
+        if novo_preco < 0:
+            raise ValueError("Preço não pode ser negativo")
+
+        self._preco = novo_preco
 
     def adicionar_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        self._quantidade += quantidade
 
     def remover_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        if quantidade > self._quantidade:
+            raise ValueError(
+                "Quantidade a ser removida é maior que a quantidade em estoque"
+            )
+
+        self._quantidade -= quantidade
